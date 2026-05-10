b0ec14218ee3eaa216964cd6866dabbc5e7a56ed
elrobe <eliasroberg@gmail.com>
2026-05-10 13:16:46 -0400

    Add meds page for mom

---
+++ pages/meds/data.js
@@ -0,0 +1,14 @@
+const DEFAULT_MEDICATIONS = [{
+  "name": "sulcrafata",
+  "for": "",
+  "dose": {
+    "amount": 1,
+    "unit": "g"
+  },
+  "dosage": "1 tablet",
+  "frequency": {
+    "prn?": false,
+    "amount": 4
+  },
+  "instructions": "Before meals and nightly"
+}];
